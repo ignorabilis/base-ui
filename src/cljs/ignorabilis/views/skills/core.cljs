76853853108417bb4e76c1fb1d07a912b8060d7c
ignorabilis <irina.yaroslavova@ignorabilis.com>
2016-08-22 18:49:49 +0300

    Improve search - make it case insensitive

--- src/cljs/ignorabilis/views/skills/core.cljs
+++ src/cljs/ignorabilis/views/skills/core.cljs
@@ -1,32 +1,41 @@
 (ns ignorabilis.views.skills.core
-  (:require [reagent.core :as r :refer [atom]]
+  (:require [reagent.core :as r]
             [clojure.string :as string]))
 
-(defonce skills-state (atom {:search "" :skills [{:label "Foundation"}
-                                                 {:label "Bootstrap"}
-                                                 {:label "C#"}]}))
+(defonce skills-state (r/atom {:query "" :skills [{:label "Foundation"}
+                                                  {:label "Bootstrap"}
+                                                  {:label "C#"}]}))
+
+(defn case-insensitive-search [query-str coll item-key-path]
+  (if (string/blank? query-str)
+    coll
+    (let [q (string/lower-case query-str)]
+      (filter
+        #(string/includes?
+          (string/lower-case
+            (get-in % item-key-path)) q)
+        coll))))
 
 (defn skill-item [{:keys [label]}]
   [:li label])
 
-(defn skills-list [skills search]
-  (let [skill-result (filter #(string/includes? (:label %) search) skills)]
+(defn skills-list [query-str skills]
+  (let [skills-result (case-insensitive-search query-str skills [:label])]
     [:ul
-     (for [{:keys [label] :as skill} skill-result]
+     (for [{:keys [label] :as skill} skills-result]
        ^{:key label} [skill-item skill])]))
 
-(defn skills-search [search]
+(defn skills-search [query-str]
   [:input
-   {:type "text"
+   {:type        "text"
     :placeholder "Search for a skill..."
-    :value search
-    :on-change (fn [e]
-                 (let [new-value (.. e -target -value)]
-                   (prn new-value)
-                   (swap! skills-state assoc-in [:search] new-value)))}])
+    :value       query-str
+    :on-change   (fn [e]
+                   (let [new-value (.. e -target -value)]
+                     (swap! skills-state assoc-in [:query] new-value)))}])
 
 (defn skills-page []
-  (let [{:keys [skills search]} @skills-state]
+  (let [{:keys [skills query]} @skills-state]
     [:div
-     [skills-search search]
-     [skills-list skills search]]))
+     [skills-search query]
+     [skills-list query skills]]))
