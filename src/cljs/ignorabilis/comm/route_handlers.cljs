a8cddd30106ffcfa907c7b52cb69d4dd1094a290
ignorabilis <irina.yaroslavova@ignorabilis.com>
2016-09-17 23:56:42 +0300

    Logging broken/not-implemented pages

--- src/cljs/ignorabilis/comm/route_handlers.cljs
+++ src/cljs/ignorabilis/comm/route_handlers.cljs
@@ -3,7 +3,8 @@
             [ignorabilis.views.home.core :as home]
             [ignorabilis.views.skills.core :as skills]
             [ignorabilis.views.not-found.core :as not-found]
-            [ignorabilis.views.not-implemented.core :as not-implemented]))
+            [ignorabilis.views.not-implemented.core :as not-implemented]
+            [taoensso.timbre :as timbre]))
 
 (def route-handlers
   {::wroutes/home-page      home/home-page
@@ -11,4 +12,8 @@
    ::wroutes/not-found-page not-found/not-found-page})
 
 (defn handler-key->current-page [handler-key]
-  (get route-handlers handler-key not-implemented/not-implemented-page))
+  (let [current-page (get route-handlers handler-key)]
+    (if current-page
+      current-page
+      (do (timbre/error "Page not implemented or broken for key: " handler-key)
+          not-implemented/not-implemented-page))))
